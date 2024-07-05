f51cd2b62ef72a4a256ca99865470a6ec6da67d5
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-05 21:37:11 +0530

    update Order

--- src/pages/OrderSummary.js
+++ src/pages/OrderSummary.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import ItemSelection from '../components/ItemSelection';
 
 const OrderSummary = () => {
     const location = useLocation();
-    const navigate = useNavigate();
-    const selectedItems  = location.state.key;
+    const history = useHistory();
+    const { selectedItems } = location.state || [];
 
     const handleTrackOrder = orderId => {
-        navigate("/track-order", { state: { orderId } })
+        history.push('/track-order', { orderId });
     };
 
     return (
-        <ItemSelection selectedItems={selectedItems} onPlaceOrder={() => handleTrackOrder(1234)} />
+        <div className="bg-white min-h-screen p-4">
+            <ItemSelection selectedItems={selectedItems} onPlaceOrder={() => handleTrackOrder(1234)} />
+        </div>
     );
 };
 
