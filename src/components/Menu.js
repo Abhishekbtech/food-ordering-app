050b531dff99c321aabd7dfd53fcfb84c9b73fd2
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-05 21:15:04 +0530

    add menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Menu = ({ menuItems, onSelectItem }) => {
+    
     return (
         <div className="p-4">
             <h1 className="text-2xl font-bold mb-4">Menu</h1>
