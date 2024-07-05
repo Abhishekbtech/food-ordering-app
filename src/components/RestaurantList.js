0b552e098250200cac7bda6eccb08896df2d1475
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-05 21:16:35 +0530

    add Restaurant

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const RestaurantList = ({ restaurants }) => {
+    
     return (
         <div className="p-4">
             <h1 className="text-2xl font-bold mb-4">Available Restaurants</h1>
