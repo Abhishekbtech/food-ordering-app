5ba8cdcb9743201421fa0b697cf3c918e0d4d069
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-05 21:13:56 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import RestaurantDetails from './pages/RestaurantDetails';
+import OrderSummary from './pages/OrderSummary';
+import TrackOrder from './pages/TrackOrder';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/restaurant/:id" element={<RestaurantDetails/>} />
+        <Route path="/order-summary" element={<OrderSummary/>} />
+        <Route path="/track-order" element={<TrackOrder/>} />
+      </Routes>
+    </Router>
   );
 }
 
